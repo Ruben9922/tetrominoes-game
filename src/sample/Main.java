c851bb7fe53e31f4b378281188575d0ea6d1c0c4
Ruben9922 <Ruben9922@users.noreply.github.com>
2017-09-21 12:50:31 +0100

    Create canvas

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,22 +1,35 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Tetrominoes Game");
+
+        Group root = new Group();
+        Canvas canvas = new Canvas(600, 800);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        draw(gc);
+        root.getChildren().add(canvas);
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    private void draw(GraphicsContext gc) {
+        gc.setFill(Color.BLUE);
+        gc.setStroke(Color.GREEN);
+        gc.setLineWidth(5);
+        gc.strokeLine(0, 0, 600, 800);
     }
 }
